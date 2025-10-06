55f36bc53dc82f38013a955c49288beb1d21a594
SiddheshSoni <work.siddheshsoni@gmail.com>
2025-10-06 12:01:14 +0530

    added functionality to change password

--- src/components/Profile/ProfileForm.jsx
+++ src/components/Profile/ProfileForm.jsx
@@ -1,14 +1,67 @@
+import { useContext, useRef, useState} from 'react';
 import classes from './ProfileForm.module.css';
+import AuthContext from '../../store/AuthenticationContext';
 
 const ProfileForm = () => {
+  const {token, onLogout} = useContext(AuthContext);
+  const passwordInputRef = useRef();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError(null);
+
+    const newPassword = passwordInputRef.current.value;
+
+    try{
+      const res = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCsfWy_iELOhjwPgk7wmq8k2Ldly3PqApY', 
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            idToken: token,
+            password: newPassword,
+            returnSecureToken: true
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      );
+
+      const data = await res.json();
+      
+      if(res.ok){
+        setIsLoading(false);
+        alert('Password changed successfully!');
+        onLogout();
+        console.log(data);
+      }
+      else{
+        let errorMessage = 'Authentication failed!';
+        if(data && data.error && data.error.message){
+          errorMessage = data.error.message;
+        }
+        throw new Error(errorMessage)
+      }
+    }
+    catch(err){
+      setIsLoading(false);
+      setError(err);
+      console.log(err);
+    }
+  }
+  
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' ref={passwordInputRef}/>
       </div>
       <div className={classes.action}>
-        <button>Change Password</button>
+        {isLoading ? <p>Sending request...</p> : <button type='submit'>Change Password</button>}
+        {error && alert(error)}
       </div>
     </form>
   );
